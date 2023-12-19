e37d767bedef6a5b042c73a5a53346047a39f249
Wing Chung Leung <lwchkg@gmail.com>
2023-12-19 18:48:36 +0000

    test: Add e2e test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -24,11 +24,23 @@ describe("Hello Dict app", () => {
     cy.get(".dict-item").contains(/\btest\b/i);
   });
 
-  it("Visit #/word/:word should load the dictionary entries.", () => {
+  it("Visit #/word/:word should load the dictionary entries. A second visit should not reload dictionary data.", () => {
     cy.visit("/#word/test");
 
     // The dictionary entries should load with an entry with the word.
     // Comparison is not case-sensitive because GCIDE capitalizes headwords.
     cy.get(".dict-item").contains(/\btest\b/i);
+
+    cy.visit("#/word/study");
+    // The second visit should load instantiously.
+    cy.get(".dict-item").contains(/\bstudy\b/i, { timeout: 100 });
+  });
+
+  it("Search with an empty textbox should navigate to somewhere other than #/word/ .", () => {
+    cy.visit("/#about");
+    cy.get(".word-input button").click();
+    // The browser should navigate to a new URL (no "about").
+    cy.hash().should("not.include", "about");
+    cy.hash().should("not.include", "word");
   });
 });
